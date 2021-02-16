a28f765ad83e6f5795a92395dca6577783cb0b29
Ernez Catovic <--help>
2021-02-16 16:27:34 +0100

    finished csutom component. Noot workimg Item Update properly

--- src/views/DesertListView.vue
+++ src/views/DesertListView.vue
@@ -1,170 +1,241 @@
-<!--<template lang="html">-->
+<template lang="html">
 
 <!--  <section class="desert-list-view">-->
-<!--    <desert-list/>-->
-<!--  </section>-->
-
-<!--</template>-->
-<template>
-  <v-data-table
-      :headers="headers"
-      :items="desserts"
-      sort-by="calories"
-      class="elevation-1"
-  >
-    <template v-slot:top>
-      <v-toolbar
-          flat
-      >
-        <v-toolbar-title>My CRUD</v-toolbar-title>
-        <v-divider
-            class="mx-4"
-            inset
-            vertical
-        ></v-divider>
-        <v-spacer></v-spacer>
-        <v-dialog
-            v-model="dialog"
-            max-width="500px"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn
-                color="primary"
-                dark
-                class="mb-2"
-                v-bind="attrs"
-                v-on="on"
+  <crud-component
+        crud-form-title="Deserts"
+        :headers="headers"
+        :items="desserts"
+        sort-by="calories"
+        table-class="elevation-1"
+        :edited-item-prop="editedParentItem"
+        :default-item-prop="defaultItem"
+        @edited="editedItemOnChildEvent"
+    >
+    <template v-slot:vcardtext>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col
+                cols="12"
+                sm="6"
+                md="4"
             >
-              New Item
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="headline">{{ formTitle }}</span>
-            </v-card-title>
+              <v-text-field
+                  v-model="editedParentItem.name"
+                  label="Dessert name"
+              ></v-text-field>
+            </v-col>
+            <v-col
+                cols="12"
+                sm="6"
+                md="4"
+            >
+              <v-text-field
+                  v-model="editedParentItem.calories"
+                  label="Calories"
+              ></v-text-field>
+            </v-col>
+            <v-col
+                cols="12"
+                sm="6"
+                md="4"
+            >
+              <v-text-field
+                  v-model="editedParentItem.fat"
+                  label="Fat (g)"
+              ></v-text-field>
+            </v-col>
+            <v-col
+                cols="12"
+                sm="6"
+                md="4"
+            >
+              <v-text-field
+                  v-model="editedParentItem.carbs"
+                  label="Carbs (g)"
+              ></v-text-field>
+            </v-col>
+            <v-col
+                cols="12"
+                sm="6"
+                md="4"
+            >
+              <v-text-field
+                  v-model="editedParentItem.protein"
+                  label="Protein (g)"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+    </template>
+    </crud-component>
 
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                  >
-                    <v-text-field
-                        v-model="editedItem.name"
-                        label="Dessert name"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                  >
-                    <v-text-field
-                        v-model="editedItem.calories"
-                        label="Calories"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                  >
-                    <v-text-field
-                        v-model="editedItem.fat"
-                        label="Fat (g)"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                  >
-                    <v-text-field
-                        v-model="editedItem.carbs"
-                        label="Carbs (g)"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                  >
-                    <v-text-field
-                        v-model="editedItem.protein"
-                        label="Protein (g)"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="close"
-              >
-                Cancel
-              </v-btn>
-              <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="save"
-              >
-                Save
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-toolbar>
-    </template>
-    <template v-slot:item.actions="{ item }">
-      <v-icon
-          small
-          class="mr-2"
-          @click="editItem(item)"
-      >
-        mdi-pencil
-      </v-icon>
-      <v-icon
-          small
-          @click="deleteItem(item)"
-      >
-        mdi-delete
-      </v-icon>
-    </template>
-    <template v-slot:no-data>
-      <v-btn
-          color="primary"
-          @click="initialize"
-      >
-        Reset
-      </v-btn>
-    </template>
-  </v-data-table>
+  <!--  </section>-->
 </template>
+
+
+<!--<template>-->
+<!--  <v-data-table-->
+<!--      :headers="headers"-->
+<!--      :items="desserts"-->
+<!--      sort-by="calories"-->
+<!--      class="elevation-1"-->
+<!--  >-->
+<!--    <template v-slot:top>-->
+<!--      <v-toolbar-->
+<!--          flat-->
+<!--      >-->
+<!--        <v-toolbar-title>My CRUD</v-toolbar-title>-->
+<!--        <v-divider-->
+<!--            class="mx-4"-->
+<!--            inset-->
+<!--            vertical-->
+<!--        ></v-divider>-->
+<!--        <v-spacer></v-spacer>-->
+<!--        <v-dialog-->
+<!--            v-model="dialog"-->
+<!--            max-width="500px"-->
+<!--        >-->
+<!--          <template v-slot:activator="{ on, attrs }">-->
+<!--            <v-btn-->
+<!--                color="primary"-->
+<!--                dark-->
+<!--                class="mb-2"-->
+<!--                v-bind="attrs"-->
+<!--                v-on="on"-->
+<!--            >-->
+<!--              New Item-->
+<!--            </v-btn>-->
+<!--          </template>-->
+<!--          <v-card>-->
+<!--            <v-card-title>-->
+<!--              <span class="headline">{{ formTitle }}</span>-->
+<!--            </v-card-title>-->
+
+<!--            <v-card-text>-->
+<!--              <v-container>-->
+<!--                <v-row>-->
+<!--                  <v-col-->
+<!--                      cols="12"-->
+<!--                      sm="6"-->
+<!--                      md="4"-->
+<!--                  >-->
+<!--                    <v-text-field-->
+<!--                        v-model="editedItem.name"-->
+<!--                        label="Dessert name"-->
+<!--                    ></v-text-field>-->
+<!--                  </v-col>-->
+<!--                  <v-col-->
+<!--                      cols="12"-->
+<!--                      sm="6"-->
+<!--                      md="4"-->
+<!--                  >-->
+<!--                    <v-text-field-->
+<!--                        v-model="editedItem.calories"-->
+<!--                        label="Calories"-->
+<!--                    ></v-text-field>-->
+<!--                  </v-col>-->
+<!--                  <v-col-->
+<!--                      cols="12"-->
+<!--                      sm="6"-->
+<!--                      md="4"-->
+<!--                  >-->
+<!--                    <v-text-field-->
+<!--                        v-model="editedItem.fat"-->
+<!--                        label="Fat (g)"-->
+<!--                    ></v-text-field>-->
+<!--                  </v-col>-->
+<!--                  <v-col-->
+<!--                      cols="12"-->
+<!--                      sm="6"-->
+<!--                      md="4"-->
+<!--                  >-->
+<!--                    <v-text-field-->
+<!--                        v-model="editedItem.carbs"-->
+<!--                        label="Carbs (g)"-->
+<!--                    ></v-text-field>-->
+<!--                  </v-col>-->
+<!--                  <v-col-->
+<!--                      cols="12"-->
+<!--                      sm="6"-->
+<!--                      md="4"-->
+<!--                  >-->
+<!--                    <v-text-field-->
+<!--                        v-model="editedItem.protein"-->
+<!--                        label="Protein (g)"-->
+<!--                    ></v-text-field>-->
+<!--                  </v-col>-->
+<!--                </v-row>-->
+<!--              </v-container>-->
+<!--            </v-card-text>-->
+
+<!--            <v-card-actions>-->
+<!--              <v-spacer></v-spacer>-->
+<!--              <v-btn-->
+<!--                  color="blue darken-1"-->
+<!--                  text-->
+<!--                  @click="close"-->
+<!--              >-->
+<!--                Cancel-->
+<!--              </v-btn>-->
+<!--              <v-btn-->
+<!--                  color="blue darken-1"-->
+<!--                  text-->
+<!--                  @click="save"-->
+<!--              >-->
+<!--                Save-->
+<!--              </v-btn>-->
+<!--            </v-card-actions>-->
+<!--          </v-card>-->
+<!--        </v-dialog>-->
+<!--        <v-dialog v-model="dialogDelete" max-width="500px">-->
+<!--          <v-card>-->
+<!--            <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>-->
+<!--            <v-card-actions>-->
+<!--              <v-spacer></v-spacer>-->
+<!--              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>-->
+<!--              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>-->
+<!--              <v-spacer></v-spacer>-->
+<!--            </v-card-actions>-->
+<!--          </v-card>-->
+<!--        </v-dialog>-->
+<!--      </v-toolbar>-->
+<!--    </template>-->
+<!--    <template v-slot:item.actions="{ item }">-->
+<!--      <v-icon-->
+<!--          small-->
+<!--          class="mr-2"-->
+<!--          @click="editItem(item)"-->
+<!--      >-->
+<!--        mdi-pencil-->
+<!--      </v-icon>-->
+<!--      <v-icon-->
+<!--          small-->
+<!--          @click="deleteItem(item)"-->
+<!--      >-->
+<!--        mdi-delete-->
+<!--      </v-icon>-->
+<!--    </template>-->
+<!--    <template v-slot:no-data>-->
+<!--      <v-btn-->
+<!--          color="primary"-->
+<!--          @click="initialize"-->
+<!--      >-->
+<!--        Reset-->
+<!--      </v-btn>-->
+<!--    </template>-->
+<!--  </v-data-table>-->
+<!--</template>-->
 
 
 <script lang="js">
 
+  import CrudComponent from "@/components/CrudComponent";
 
 export default {
   name: 'desert-list-view',
-  components: {},
+  components: {CrudComponent},
   props: [],
   mounted() {
 
@@ -187,7 +258,7 @@ export default {
     ],
     desserts: [],
     editedIndex: -1,
-    editedItem: {
+    editedParentItem: {
       name: '',
       calories: 0,
       fat: 0,
@@ -200,28 +271,31 @@ export default {
       fat: 0,
       carbs: 0,
       protein: 0,
-    },
+    }
   }),
-  computed: {
-    formTitle() {
-      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
-    },
-  },
+  // computed: {
+  //   formTitle() {
+  //     return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+  //   },
+  // },
 
-  watch: {
-    dialog(val) {
-      val || this.close()
-    },
-    dialogDelete(val) {
-      val || this.closeDelete()
-    },
-  },
+  // watch: {
+  //   dialog(val) {
+  //     val || this.close()
+  //   },
+  //   dialogDelete(val) {
+  //     val || this.closeDelete()
+  //   },
+  // },
 
   created() {
     this.initialize()
   },
 
   methods: {
+    editedItemOnChildEvent(item) {
+      this.editedParentItem = item
+    },
     initialize() {
       this.desserts = [
         {
@@ -297,47 +371,47 @@ export default {
       ]
     },
 
-    editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
-    },
+    // editItem(item) {
+    //   this.editedIndex = this.desserts.indexOf(item)
+    //   this.editedItem = Object.assign({}, item)
+    //   this.dialog = true
+    // },
+    //
+    // deleteItem(item) {
+    //   this.editedIndex = this.desserts.indexOf(item)
+    //   this.editedItem = Object.assign({}, item)
+    //   this.dialogDelete = true
+    // },
+    //
+    // deleteItemConfirm() {
+    //   this.desserts.splice(this.editedIndex, 1)
+    //   this.closeDelete()
+    // },
 
-    deleteItem(item) {
-      this.editedIndex = this.desserts.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialogDelete = true
-    },
-
-    deleteItemConfirm() {
-      this.desserts.splice(this.editedIndex, 1)
-      this.closeDelete()
-    },
-
-    close() {
-      this.dialog = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
-    },
-
-    closeDelete() {
-      this.dialogDelete = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
-    },
-
-    save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem)
-      } else {
-        this.desserts.push(this.editedItem)
-      }
-      this.close()
-    },
+    // close() {
+    //   this.dialog = false
+    //   this.$nextTick(() => {
+    //     this.editedItem = Object.assign({}, this.defaultItem)
+    //     this.editedIndex = -1
+    //   })
+    // },
+    //
+    // closeDelete() {
+    //   this.dialogDelete = false
+    //   this.$nextTick(() => {
+    //     this.editedItem = Object.assign({}, this.defaultItem)
+    //     this.editedIndex = -1
+    //   })
+    // },
+    //
+    // save() {
+    //   if (this.editedIndex > -1) {
+    //     Object.assign(this.desserts[this.editedIndex], this.editedItem)
+    //   } else {
+    //     this.desserts.push(this.editedItem)
+    //   }
+    //   this.close()
+    // },
   },
 }
 
